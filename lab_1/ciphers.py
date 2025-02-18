fccb5ba96f0009819814cdb8afbfd9ecfab3779b
Big Brother <aykibugz@yandex.ru>
2025-02-18 18:30:42 +0400

    Add caesar cipher

--- lab_1/ciphers.py
+++ lab_1/ciphers.py
@@ -1,4 +1,13 @@
 ﻿def substitution(alphabet1: str, alphabet2: str, message: str) -> str:
+    if len(alphabet1) != len(set(alphabet1)):
+        raise ValueError("Alphabet1 contains the same characters")
+    
+    if len(alphabet2) != len(set(alphabet2)):
+        raise ValueError("Alphabet2 contains the same characters")
+
+    if len(alphabet1) > len(alphabet2):
+        raise ValueError("Alphabet2 len less then Alphabet1 len")
+    
     cipher_text = ""
 
     for char in message:
@@ -9,7 +18,25 @@
         
         cipher_text += alphabet2[idx]
     
-    return message
+    return cipher_text
 
-def caesar(alphabet: str, shift: int, message: str) -> str:
-    pass
+def caesar(alphabet: str, message: str, shift: int = 3) -> str:
+    cipher_text = ""
+
+    for char in message:
+        idx = alphabet.find(char)
+        if idx == -1:
+            cipher_text += char
+            continue
+
+        new_idx = (idx + shift) % len(alphabet)
+        cipher_text += alphabet[new_idx]
+
+    return cipher_text
+
+
+if __name__ == "__main__":
+    text = "abcdef"
+    cipher_text = caesar(text, text, 10)
+
+    print(cipher_text)
