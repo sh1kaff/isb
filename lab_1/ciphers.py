b2c47b9fc252bf2c63621f75dc139dac199cd9cd
Big Brother <aykibugz@yandex.ru>
2025-02-18 18:15:58 +0400

    Add substitutuin cipher

---
+++ lab_1/ciphers.py
@@ -0,0 +1,15 @@
+﻿def substitution(alphabet1: str, alphabet2: str, message: str) -> str:
+    cipher_text = ""
+
+    for char in message:
+        idx = alphabet1.find(char)
+        if idx == -1:
+            cipher_text += char
+            continue
+        
+        cipher_text += alphabet2[idx]
+    
+    return message
+
+def caesar(alphabet: str, shift: int, message: str) -> str:
+    pass
